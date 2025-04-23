a39017bc35bc09ba2cb129e37448ad044460872e
Diego Cabral <145566485+di21c12@users.noreply.github.com>
2025-04-23 08:26:03 -0300

    atualização

--- jogo2dl/Assets/Scripts/Player.cs
+++ jogo2dl/Assets/Scripts/Player.cs
@@ -13,11 +13,13 @@ public class Player : MonoBehaviour
     public bool doubleJump;
 
     private Rigidbody2D rig;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
@@ -30,6 +32,24 @@ public class Player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+        
+        if(Input.GetAxis("Horizontal") > 0f )
+        {
+            anim.SetBool("walk" , true);
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+
+         if(Input.GetAxis("Horizontal") < 0f )
+        {
+            anim.SetBool("walk" , true);
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+
+         if(Input.GetAxis("Horizontal") == 0f )
+        {
+            anim.SetBool("walk" , false);
+        }
+        
     }
 
     void Jump()
